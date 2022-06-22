a5dfa0dcdad4097c78b750436d3ac17fb4838318
zed-web-m <zed-web-m@github.com>
2022-06-22 14:49:02 +0300

    technology section mobile

---
+++ js/technology-slider.js
@@ -0,0 +1,13 @@
+const technologySlider = new Swiper(".technologySlider", {
+  loop: true,
+  slidesPerView: 1,
+  autoHeight: true,
+  effect: "fade",
+  autoplay: {
+    delay: 2500,
+  },
+  pagination: {
+    el: ".technology-pagination",
+    clickable: true,
+  },
+});
